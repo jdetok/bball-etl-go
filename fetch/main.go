da4a26de2f6fc29584081b372b0b76dda37f1204
jdetok <jdeko17@gmail.com>
2025-07-24 17:40:31 -0500

    added more endpoints

--- fetch/main.go
+++ fetch/main.go
@@ -7,32 +7,44 @@ import (
 
 var commonPlayerInfo = GetReq{
 	Host:     HOST,
-	Endpoint: "/stats/commonplayerinfo",
 	Headers:  HDRS,
+	Endpoint: "/stats/commonplayerinfo",
 	Params:   []Pair{{"LeagueID", "10"}, {"PlayerID", "2544"}},
 }
 
 var playerAwards = GetReq{
 	Host:     HOST,
-	Endpoint: "/stats/playerawards",
 	Headers:  HDRS,
+	Endpoint: "/stats/playerawards",
 	Params:   []Pair{{"PlayerID", "2544"}},
 }
 
 var commonAllPlayers = GetReq{
 	Host:     HOST,
-	Endpoint: "/stats/commonallplayers",
 	Headers:  HDRS,
+	Endpoint: "/stats/commonallplayers",
 	Params: []Pair{
 		{"LeagueID", "00"},
 		{"IsOnlyCurrentSeason", "1"},
 		{"Season", "2024-25"}},
 }
 
+var leagueStandings = GetReq{
+	Host:     HOST,
+	Headers:  HDRS,
+	Endpoint: "/stats/leaguestandings",
+	Params: []Pair{
+		{"LeagueID", "00"},
+		{"Season", "2024-25"},
+		// {"SeasonType", "Playoffs"}},
+		{"SeasonType", "Regular+Season"}},
+}
+
 func main() {
 	// body, _, err := commonPlayerInfo.GetRespBody()
 	// body, _, err := commonAllPlayers.GetRespBody()
-	body, _, err := playerAwards.GetRespBody()
+	// body, _, err := playerAwards.GetRespBody()
+	body, _, err := leagueStandings.GetRespBody()
 	if err != nil {
 		log.Fatal(err)
 	}
