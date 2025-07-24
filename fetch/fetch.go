05fa0216fc8b3c01702ce77809db8925531724bd
jdetok <jdeko17@gmail.com>
2025-07-24 16:34:28 -0500

    split up files

--- fetch/fetch.go
+++ fetch/fetch.go
@@ -3,7 +3,6 @@ package main
 import (
 	"fmt"
 	"io"
-	"log"
 	"net/http"
 )
 
@@ -68,25 +67,4 @@ func Get(host string, end string, params []Pair, hdrs []Pair) ([]byte, int, erro
 		return nil, status, fmt.Errorf("%d: HTTP Request Error: %e", status, err)
 	}
 	return body, status, nil
-}
-
-func main() {
-	var hdrs = []Pair{
-		{"Accept", "application/json"},
-		{"Connection", "keep-alive"},
-		{"Referer", "https://www.nba.com"},
-		{"Origin", "https://www.nba.com"},
-		{"User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36"},
-	}
-
-	var params = []Pair{
-		{"LeagueID", "10"},
-		{"PlayerID", "2544"},
-	}
-
-	body, _, err := Get("stats.nba.com", "/stats/commonplayerinfo", params, hdrs)
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println(string(body))
 }
---
+++ fetch/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"log"
+)
+
+var hdrs = []Pair{
+	{"Accept", "application/json"},
+	{"Connection", "keep-alive"},
+	{"Referer", "https://www.nba.com"},
+	{"Origin", "https://www.nba.com"},
+	{"User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36"},
+}
+
+var params = []Pair{
+	{"LeagueID", "10"},
+	{"PlayerID", "2544"},
+}
+
+func main() {
+	body, _, err := Get("stats.nba.com", "/stats/commonplayerinfo", params, hdrs)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(string(body))
+}
