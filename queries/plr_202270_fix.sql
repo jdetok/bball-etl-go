7925894512f489884288ba426ed622dbc94c43ec
jdetok <jdeko17@gmail.com>
2025-08-05 14:44:09 -0500

    recovery, attempting new build on 5437

--- queries/plr_202270_fix.sql
+++ queries/plr_202270_fix.sql
@@ -3,6 +3,7 @@ INSERT A ROW INTO lg.plr FOR WNBA PLAYER ANGEL ROBINSON WITH PLAYER ID 202270
 this player had the ID 202270 in 2014 and 202657 in all years after
 this was causing an error with loading stats.pbox table
 create a new record with identical data except player id
+MUST BE RUN AFTER THE lg.sp_player_all_load
 */
 
 insert into lg.plr (lg_id, player_id, plr_cde, player, last_first, from_year, to_year)
